0f48014dd53a50fe7d3d8ee06d894bd440e7d8cf
Athimet Aiewcharoen <mobiltiy2604@gmail.com>
2022-05-26 08:25:09 +0700

    set home

--- com/Core/Src/main.c
+++ com/Core/Src/main.c
@@ -1,4 +1,6 @@
 /* USER CODE BEGIN Header */
+// Code by Athimet
+// Write at 70 BangWaek3 Phasi Charoen 10160 [Athimet home]
 /**
   ******************************************************************************
   * @file           : main.c
